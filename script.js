a58794917d1546e6f661963daab69e4622aa2dae
Vojtěch Vlasák <112420135+Vojtavlas@users.noreply.github.com>
2023-11-07 19:28:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 const fileInput = document.getElementById('fileInput');
 const encryptButton = document.getElementById('encryptButton');
 const downloadLink = document.getElementById('downloadLink');
+const keyDisplay = document.getElementById('keyDisplay');
+const copyKeyButton = document.getElementById('copyKeyButton');
 let selectedFile;
 
 fileInput.addEventListener('change', (event) => {
     selectedFile = event.target.files[0];
     if (selectedFile) {
         encryptButton.disabled = false;
+        copyKeyButton.disabled = true;
+        keyDisplay.textContent = 'Key: Hard-codedDemoKey';
     } else {
         encryptButton.disabled = true;
+        copyKeyButton.disabled = true;
+        keyDisplay.textContent = '';
     }
 });
 
@@ -17,19 +23,25 @@ encryptButton.addEventListener('click', async () => {
         return;
     }
 
-    const password = prompt('Enter a password for encryption:');
-    if (!password) {
-        return;
-    }
+    const keyHex = 'Hard-codedDemoKey';
 
-    const fileBuffer = await selectedFile.arrayBuffer();
-    const cryptoKey = await window.crypto.subtle.importKey('raw', new TextEncoder().encode(password), 'AES-CBC', false, ['encrypt']);
     const iv = window.crypto.getRandomValues(new Uint8Array(16));
-    const encryptedData = await window.crypto.subtle.encrypt({ name: 'AES-CBC', iv }, cryptoKey, fileBuffer);
+    const fileBuffer = await selectedFile.arrayBuffer();
+    const encryptedData = await window.crypto.subtle.encrypt({ name: 'AES-CBC', iv }, new TextEncoder().encode(keyHex), fileBuffer);
 
     const encryptedBlob = new Blob([iv, encryptedData], { type: 'application/octet-stream' });
     const encryptedFileName = `${selectedFile.name}.aes`;
     downloadLink.href = URL.createObjectURL(encryptedBlob);
-    downloadLink.download = encryptedFileName; // Set the download attribute
+    downloadLink.download = encryptedFileName;
     downloadLink.style.display = 'block';
+});
+
+copyKeyButton.addEventListener('click', () => {
+    const keyHex = 'Hard-codedDemoKey';
+    const textarea = document.createElement('textarea');
+    textarea.value = keyHex;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
 });
