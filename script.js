6203efcab83c780eda98eb364d1f17b0709db888
Vojtěch Vlasák <112420135+Vojtavlas@users.noreply.github.com>
2023-11-07 17:00:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 const fileInput = document.getElementById('fileInput');
+const newFileNameInput = document.getElementById('newFileName');
+const downloadButton = document.getElementById('downloadButton');
 const fileInfo = document.getElementById('fileInfo');
 
+let selectedFile;
+
 fileInput.addEventListener('change', (event) => {
-    const file = event.target.files[0];
-    if (file) {
-        const fileName = file.name;
-        const fileSize = Math.round(file.size / 1024) + ' KB';
-        const fileType = file.type || 'n/a';
+    selectedFile = event.target.files[0];
+    if (selectedFile) {
+        const fileName = selectedFile.name;
+        const fileSize = Math.round(selectedFile.size / 1024) + ' KB';
+        const fileType = selectedFile.type || 'n/a';
         fileInfo.textContent = `File Name: ${fileName}\nFile Size: ${fileSize}\nFile Type: ${fileType}`;
+        downloadButton.disabled = false;
     } else {
         fileInfo.textContent = 'No file selected';
+        downloadButton.disabled = true;
+    }
+});
+
+downloadButton.addEventListener('click', () => {
+    if (selectedFile) {
+        const newFileName = newFileNameInput.value || selectedFile.name;
+        const downloadLink = document.createElement('a');
+        downloadLink.href = URL.createObjectURL(selectedFile);
+        downloadLink.download = newFileName;
+        downloadLink.click();
     }
 });
