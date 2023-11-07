ec3386acbc542a62c259023a09b44aabc7c14674
Vojtěch Vlasák <112420135+Vojtavlas@users.noreply.github.com>
2023-11-07 16:55:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,14 @@
-const fileInput = document.getElementById("file-input");
-const fileInfo = document.getElementById("file-info");
-const fileName = document.getElementById("file-name");
-const fileType = document.getElementById("file-type");
-const fileSize = document.getElementById("file-size");
-const renameButton = document.getElementById("rename-button");
-const downloadButton = document.getElementById("download-button");
+const fileInput = document.getElementById('fileInput');
+const fileInfo = document.getElementById('fileInfo');
 
-document.getElementById("file-upload-form").addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const formData = new FormData();
-    formData.append("file", fileInput.files[0]);
-
-    try {
-        const response = await fetch("/upload", {
-            method: "POST",
-            body: formData,
-        });
-
-        if (response.ok) {
-            displayFileInfo();
-        }
-    } catch (error) {
-        console.error("Error uploading file: " + error);
-    }
-});
-
-renameButton.addEventListener("click", async () => {
-    try {
-        const response = await fetch("/rename", {
-            method: "GET",
-        });
-
-        if (response.ok) {
-            alert("File renamed successfully");
-        }
-    } catch (error) {
-        console.error("Error renaming file: " + error);
-    }
-});
-
-downloadButton.addEventListener("click", () => {
-    window.location.href = "/download";
-});
-
-function displayFileInfo() {
-    const file = fileInput.files[0];
+fileInput.addEventListener('change', (event) => {
+    const file = event.target.files[0];
     if (file) {
-        fileName.textContent = file.name;
-        fileType.textContent = file.type;
-        fileSize.textContent = (file.size / 1024).toFixed(2) + " KB";
+        const fileName = file.name;
+        const fileSize = Math.round(file.size / 1024) + ' KB';
+        const fileType = file.type || 'n/a';
+        fileInfo.textContent = `File Name: ${fileName}\nFile Size: ${fileSize}\nFile Type: ${fileType}`;
     } else {
-        fileInfo.innerHTML = "<p>No file selected.</p>";
+        fileInfo.textContent = 'No file selected';
     }
-}
+});
