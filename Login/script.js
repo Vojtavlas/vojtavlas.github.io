d744cc6e33cccf8da75340049868643c97cb3d20
vojtavlas <vojtavlas@gmail.com>
2023-11-11 21:24:59 +0100

    asd

--- Login/script.js
+++ Login/script.js
@@ -3,15 +3,24 @@ function validateLogin() {
     var password = document.getElementById("password").value;
     var errorMessage = document.getElementById("error-message");
 
-    // Simple validation (you can replace this with your own authentication logic)
-    if (username === "user" && password === "password") {
-        // Successful login
-        errorMessage.innerHTML = "";
+    // Fetch user data from a JSON file (replace with your JSON file path)
+    fetch('users.json')
+        .then(response => response.json())
+        .then(users => {
+            // Simulated authentication (replace with your authentication logic)
+            const user = users.find(u => u.username === username && u.password === password);
 
-        // Redirect to the specified URL
-        window.location.href = "https://vojtavlas.github.io/Aes";
-    } else {
-        // Failed login
-        errorMessage.innerHTML = "Invalid username or password";
-    }
+            if (user) {
+                // Successful login
+                errorMessage.innerHTML = "";
+                alert("Login successful! Redirect to your desired page.");
+            } else {
+                // Failed login
+                errorMessage.innerHTML = "Invalid username or password";
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching user data:', error);
+            errorMessage.innerHTML = "Error fetching user data";
+        });
 }
